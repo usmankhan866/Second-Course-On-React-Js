09470f626454431917f525461917678357a63bf3
Mohammad Usman <59081139+usmankhan866@users.noreply.github.com>
2021-09-14 11:06:01 +0500

    React Js Hooks

--- App.js
+++ App.js
@@ -1,22 +1,47 @@
-import React from "react"
-import {add, sub, mult, div} from './Calculator';
+import React, { useState } from 'react';
 
+/* React Hooks: it was introduced in react version of 16.8
+   They let you use of state and other React features without writing a class.
+   it is only used inside the top of react functional components (not the oustide).
+   React Hook "useState" cannot be called at the top level. React Hooks must be called in a 
+   React function component or a custom React Hook function 
+*/
 
-function App(){
+let count = 1;
+ 
+const App = ()=>{
+
+  /* useState() method: It will return an array containing two items 1:undefined (which is a state object also called current data)
+     2: function (which is called updated function also called updated data).
+     if u pass any value/data to useState its called intial Data which is equal to intial Data = 
+     current Data , updated Data.
+   */   
+  
+  const state = useState();
+  // console.log(state);
+
+  /* if we pass any value to useState() it will be the current value of count (state). and setCount is function whose
+     value will be the update value of count state (mean if we assign a value to setCount so the count state value will be
+      change from intial value (mean from value of useState) to value setCount function)
+  */    
+//  Array Destructuring
+  const [count, setCount] = useState(0);
+
+const IncrMnt = () =>{
+
+  setCount(count + 1);
+  // console.log('clicked ' + count++);
+}
 
   return(
-<div>
-     <ol>
-      <li>The sum of two no is  { add(40, 4) } </li>
-      <li>The sub of two no is  { sub(30, 3) } </li>
-      <li>The Div of two no is  { div(10, 3) } </li>
-      <li>The mult of two no is { mult(10, 3) } </li>
-     </ol>
-  </div>
-);
 
-  }
+    <> 
+    
+         <div> {count} </div>
+         <button onClick={IncrMnt}> Click Me </button>
+    </>
+  );
+}
 
-  export default App;
 
-   
+export default App;
--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// Now i import the new component Heading
 import App from './App'
 
 
 ReactDOM.render(
-  <App />, document.getElementById ('root') );
+
+  // Here i mentioned how to write in the DOM, but not mentioned where to write in the DOM.
+  /* Now this Heading JSX element is our customized element created through component */  
+  
+  <App />,
+  // Now i mentioned where to write in the DOM
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+import './index.css';
+import App from './App';
+
+
+
+ReactDOM.render( <App />,  document.getElementById('root') );
 
 
