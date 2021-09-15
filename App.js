acf415696c73b4b739dfacbaf99f0720d56ff928
Mohammad Usman <59081139+usmankhan866@users.noreply.github.com>
2021-09-15 14:28:01 +0500

    Event Handling

--- App.js
+++ App.js
@@ -1,70 +1,71 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, {useState} from "react";
 
-// Adding Lifecycle Methods to a Class
 
-class Clock extends React.Component{
+// const App = ()=>{
 
-  constructor(props){
-    super(props);
+//   const oldCol = 'mediumpurple';
+//   const [bgCol, setCol] = useState(oldCol);
 
-    this.state = {date: new Date()};
+//   const [name, setName] = useState('click me');
+
+//   const bgChange = ()=>{
+//    let newCol = 'yellow';
+//    setCol(newCol);
+//    setName('ooh!! :');
+//     // console.log('clicked');
+
+//   }
+
+//   const oldBg = ()=>{
+
+//     setCol(oldCol);
+//     setName("wawoo!!  ");
+//   }
+
+//   return(
+//     <>
+//        <div style={{ backgroundColor: bgCol }}>
+//             <button onClick= {bgChange} onDoubleClick={oldBg}> {name} </button>
+//        </div>
+
+//     </>
+//   );
+
+// };
+
+const App = ()=>{
+
+  const oldInfo = 'Hello World I am Mohammad Usman';
+  const [info, setInfo] = useState(oldInfo);
+  
+  let oldBg = "mediumpurple"; 
+  const [bg, setBg] = useState(oldBg);
+
+  let border = '0px';
+  const [chngBorder, setBorder] = useState(border);
+
+  const myInfo = ()=>{
+
+    setInfo('And i am the bechlor student of Computer Science');
+    setBg('maroon');
+    setBorder('3px solid');
   }
 
+  const myName = ()=>{
 
-  componentDidMount(){
-
-    this.timeID = setInterval(
-      ()=>this.tick(), 1000);
-    
+    setInfo(oldInfo);
+    setBg(oldBg);
+    setBorder(border);
   }
 
-  componentDidUnmount(){
+  return(
+    <>
 
-    clearInterval(this.timeID);
-  }
-
-  tick() {
-    
-    this.setState({
-      date: new Date()
-    });
-  }
-
-  render(){
-
-    return(
-      <div>
-
-        <h1>Hello World!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()} </h2>
-      </div>
-    );
-  }
+       <div style={{ backgroundColor: bg, border: chngBorder }}>{info} <br />
+         <button onMouseMove= {myInfo} onMouseLeave= {myName}> click me </button>
+       </div>
+    </>
+  );
 }
 
-ReactDOM.render(
-  <Clock />, document.getElementById('root')
-);
-
-export default Clock;
-
-
-/*
-
-Let’s quickly recap what’s going on and the order in which the methods are called:
-
-When <Clock /> is passed to ReactDOM.render(), React calls the constructor of the Clock component. Since Clock needs
-to display the current time, it initializes this.state with an object including the current time. We will later update
- this state.React then calls the Clock component’s render() method. This is how React learns what should be displayed
-on the screen. React then updates the DOM to match the Clock’s render output.
-When the Clock output is inserted in the DOM, React calls the componentDidMount() lifecycle method. Inside it, the Clock 
-component asks the browser to set up a timer to call the component’s tick() method once a second.
-Every second the browser calls the tick() method. Inside it, the Clock component schedules a UI update by calling 
-setState() with an object containing the current time. Thanks to the setState() call, React knows the state has changed, 
-and calls the render() method again to learn what should be on the screen. This time, this.state.date in the render() 
-method will be different, and so the render output will include the updated time. React updates the DOM accordingly.
-If the Clock component is ever removed from the DOM, React calls the componentWillUnmount() lifecycle method so the timer
-is stopped.
-
-*/
+export default App;
