83fe415f48d57004a097c5c1264edfc0ceda503d
Mohammad Usman <59081139+usmankhan866@users.noreply.github.com>
2021-09-16 20:44:20 +0500

    More Interactive React Form with more input fields

--- App.js
+++ App.js
@@ -1,71 +1,126 @@
-import React, {useState} from "react";
+import { render } from "@testing-library/react";
+import React from "react";
+import { useState } from "react/cjs/react.development";
+
 
 const App = ()=>{
-/* Here we have used the Hooks to change the states of the user enter value in the first input field.The val of
- "name" object of useState method is equal to the val of the user enter in the input field simultanously. And on 
- clicking the submit button this name object value is passed to state "firstName" because we have passed the "name" 
- value to the "setFirstName(name)" method of useState method, so the current value of "firstName" is equal to value of 
- "name"..
-*/
 
-// Hooks for changing the state of first input field
-const [name, setName] = useState();
-const [firstName, setFirstName] = useState();
+  // Here we passed an object (fName, lName) in the useState() method.. 
+  const [fullName, setFullName] = useState({
 
-// Hooks for changing the state of second input field
-const [endName, setEndName] = useState();
-const [lastName, setLastName] = useState();
+        fName: "",
+        lName: "",
+        email: "",
+        phone: "",
+  });
 
-/* To get the value of input field. "onChange" method pass an event object (it can access many mrthods) 
-to inputEvent function to get the value of input fields
-*/
-const inputEvent = (event)=>{
+  const inputEvent = (event)=>{
 
-  let eventVal = event.target.value;
-  setName(eventVal);
-};
+    // Here in the value and name we have accessed the value and name attributes of the input elements
+    // const value = event.target.value;
+    // const name  = event.target.name;
 
-/* To get the value of input field. "onChange" method pass an event object (it can access many mrthods) 
-to inputEventTwo function to get the value of input fields
-*/
-const inputEventTwo = (event)=>{
+    // Array destructuring
+    const {name, value} = event.target;
+    
+   
+    /* Note:: In the setFullName() method we can access the previous value of the input fields
+       Here we passed an object "preVal" to access the previous value of input fields
+    */  
+    setFullName((preVal)=>{
+      /* if the name attribute is equal to "fName" mean its the first input field
+         so we will get the value of fName and this value is pass to the empty object fName of useState method
+         and the previous value of lName (which is empty)
+      */  
+      if(name === "fName"){
+        return{ 
+        fName: value,
+        lName: preVal.lName,
+        email: preVal.email,
+        phone:preVal.phone,
+      };
+      }  // the same above explanation for if the name is "lName"..  
+      else if(name === "lName"){
+        return{ 
+          fName: preVal.fName,
+           lName: value,
+           email: preVal.email,
+           phone:preVal.phone,
+        };
+      }
 
-  let eventVal2 = event.target.value;
-  setEndName(eventVal2);
-};
+      else if(name === "email"){
+        return{ 
+          fName: preVal.fName,
+          lName: preVal.lName,
+          email:value,
+          phone:preVal.phone,
+        };
+      }
 
-/* when we click the on the submit the input fields values will be displayed in the h1 tags after Hello
-   it is because of using the Hooks which change the state of firstName and lastName.
-   */
-const onSubmit = ()=>{
+      else if(name === "phone"){
+        return{ 
+          fName: preVal.fName,
+          lName: preVal.lName,
+          email: preVal.email,
+          phone: value,
+        };
+      }
+    
+     });
+  }
 
-  setFirstName(name);
-  setLastName(endName);
-}
+  const onSubmits = (event)=>{
+    // To prevent the default behaviour of form
+    event.preventDefault();
+    alert("Form is submitted"); 
+  }
+
 
   return(
   <div>
+    <div className="main_div">
+     <form onSubmit={onSubmits}> 
 
-    <h1>Hello {firstName} {lastName}</h1>
-    {/* if we enter the value attribute in the input field so then whatever we enter in the input field will
-        not be appear because its controlled by react (mean controlled element).
-        To appear what we enter in the input field we have two method 1: use "defaultValue"(not usabale).
-        2: "onChange" method and pass a method/function to it (Always use this method in the input field)*/}
-    <input type='text' placeholder= 'Enter Your Name'
-    onChange= {inputEvent}
-    /* To display the input value in the h1 tag we must write the value attribute because without we can'nt display the 
-       value of input field in the h1 tag */ 
-    value= {name} />
-    <br/>
+         <h1>Hello {fullName.fName} {fullName.lName}</h1>
+         <p> {fullName.email} </p>
+         <p> {fullName.phone} </p>
 
-    <input type='text' placeholder= 'Enter Your Password'
-    onChange= {inputEventTwo} 
-    value= {endName} />
-    <br/>
-    <button onClick={onSubmit}>Submit</button>
+         <input type="text" placeholder="Enter your First name" 
+         onChange={inputEvent}
+         name="fName"
+         value= {fullName.fName}
+         />
+
+         <br />
+         <input type="text" placeholder="Enter your Last Name" 
+         onChange={inputEvent}
+         name="lName"
+         value= {fullName.lName}
+         />
+         <br />
+
+         <input type="email" placeholder="Enter your email" 
+         onChange={inputEvent}
+         name="email"
+         value= {fullName.email}
+         />
+         <br />
+
+         <input type="number" placeholder="Enter your Phone number" 
+         onChange={inputEvent}
+         name="phone"
+         value= {fullName.phone}
+         />
+         <br/>
+         
+         <button>Submit</button>
+     </form>
+
+    </div>
   </div>
   );
-}
 
+}
 
 export default App;
