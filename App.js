f43ccd0a84e2c742b263f118bb1f953fb9d7d415
Mohammad Usman <59081139+usmankhan866@users.noreply.github.com>
2021-09-17 11:11:46 +0500

    using Spread operator in React Form
    Spread syntax (...) allows an iterable such as an array expression or string to be expanded in places where zero or more arguments (for function calls) or elements (for array literals) are expected, or an object expression to be expanded in places where zero or more key-value pairs (for object literals) are expected.

--- App.js
+++ App.js
@@ -12,8 +12,10 @@ const App = ()=>{
         lName: "",
         email: "",
         phone: "",
+        qual: "",
   });
 
+  
   const inputEvent = (event)=>{
 
     // Here in the value and name we have accessed the value and name attributes of the input elements
@@ -28,44 +30,52 @@ const App = ()=>{
        Here we passed an object "preVal" to access the previous value of input fields
     */  
     setFullName((preVal)=>{
-      /* if the name attribute is equal to "fName" mean its the first input field
-         so we will get the value of fName and this value is pass to the empty object fName of useState method
-         and the previous value of lName (which is empty)
-      */  
-      if(name === "fName"){
-        return{ 
-        fName: value,
-        lName: preVal.lName,
-        email: preVal.email,
-        phone:preVal.phone,
-      };
-      }  // the same above explanation for if the name is "lName"..  
-      else if(name === "lName"){
-        return{ 
-          fName: preVal.fName,
-           lName: value,
-           email: preVal.email,
-           phone:preVal.phone,
-        };
-      }
 
-      else if(name === "email"){
-        return{ 
-          fName: preVal.fName,
-          lName: preVal.lName,
-          email:value,
-          phone:preVal.phone,
-        };
-      }
+         return{
+        /*preVal object containing the property of fName, lName, email, phone, etc if the name of preVal match 
+        to the [name] assign that value to that input field value */
+         ...preVal,
+         [name] : value,
+         };
+    
+      // if(name === "fName"){
+      //   return{ 
+      //   fName: value,
+      //  lName: preVal.lName,
+      //  email: preVal.email,
+      //  phone:preVal.phone,
+      //  
+      // };
+      // }  // the same above explanation for if the name is "lName"..  
+      // else if(name === "lName"){
+      //   return{ 
+      //     fName: preVal.fName,
+      //     lName: value,
+      //     email: preVal.email,
+      //     phone:preVal.phone,
+      //     
+      //   };
+      // }
 
-      else if(name === "phone"){
-        return{ 
-          fName: preVal.fName,
-          lName: preVal.lName,
-          email: preVal.email,
-          phone: value,
-        };
-      }
+      // else if(name === "email"){
+      //   return{ 
+      //     fName: preVal.fName,
+      //     lName: preVal.lName,
+      //     email:value,
+      //     phone:preVal.phone,
+      //    
+      //   };
+      // }
+
+      // else if(name === "phone"){
+      //   return{ 
+      //     fName: preVal.fName,
+      //     lName: preVal.lName,
+      //     email: preVal.email,
+      //      phone: value,
+      //      
+      //   };
+      // }
     
      });
   }
@@ -85,6 +95,7 @@ const App = ()=>{
          <h1>Hello {fullName.fName} {fullName.lName}</h1>
          <p> {fullName.email} </p>
          <p> {fullName.phone} </p>
+         <p> {fullName.qual}  </p>
 
          <input type="text" placeholder="Enter your First name" 
          onChange={inputEvent}
@@ -111,6 +122,13 @@ const App = ()=>{
          onChange={inputEvent}
          name="phone"
          value= {fullName.phone}
+         />
+         <br/>
+
+         <input type="text" placeholder="Enter your Qualification" 
+         onChange={inputEvent}
+         name="qual"
+         value= {fullName.qual}
          />
          <br/>
          
