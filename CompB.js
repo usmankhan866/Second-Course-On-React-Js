fdfcab1ec556ec2a16c98b233bb00cb6092178a5
Mohammad Usman <59081139+usmankhan866@users.noreply.github.com>
2021-09-22 22:46:52 +0500

    using "useContext()"  Hooks
    In this case we have used useContext() Hook method instead of long "consumer" method ( in component CompC) we just pass the data (customized jsx component Element) in the "useContext()" method to access the parent component Data.

--- CompB.js
+++ CompB.js
@@ -1,11 +1,13 @@
-import React from "react";
-import CompC from './CompC'
-
+import React, { useContext } from "react";
+import { FirstName, LastName } from "./App";
 
 const CompB =  ()=> {
-  return (
-      
-    <CompC />
+  
+    const fname = useContext(FirstName);
+    const lname = useContext(LastName);
+    
+    return (
+      <h1>This is {fname} {lname} </h1>
     );
 
 }
