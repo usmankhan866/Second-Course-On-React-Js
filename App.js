055ef0d3837974060d04d1b71fff0e6604fc396f
Mohammad Usman <59081139+usmankhan866@users.noreply.github.com>
2021-09-24 09:13:35 +0500

    React "useEffect" Hook and Challenge on it
    Using "useEffect" Hooks in react Js: It is used when u want to display something on screen after the react app will rendered (and then display something )

--- App.js
+++ App.js
@@ -1,26 +1,45 @@
-import React, { createContext } from "react";
-import CompA from './CompA'
+import React, {useEffect, useState}  from "react";
 
-// Using Context Api
+/* Using useEffect Hooks in react Js: It is used when u want to display something on screen after
+   the react app will rendered (and then display something ) */
 
-const FirstName = createContext();
-const LastName = createContext();
 
 const App =  ()=> {
 
+  const [num, setNum] = useState(0);
+  const [num2, setNum2] = useState(0);
   
+  // useEffect(()=>{
+  //   alert("I am Clicked::");
+  // });
+
+  /* Note i want that on click of button 1 alert message occure not on the click of button 2
+     for that we will pass the the array containg the current value of button 1 "[num]"
+   */
+
+     useEffect(()=>{
+
+      document.title = `You clicked me ${num} times`;
+     });
+
+    //  useEffect(()=>{
+    //   alert("I am Clicked::");
+    //   console.log("Only on button 1 is clicked this console message will appear");
+    // }, [num]);
+
   return (
     <>
-    <FirstName.Provider value= {'Mohammad'}>
-      <LastName.Provider value={'Usman'}>
-         <CompA />
-    </LastName.Provider>
-    </FirstName.Provider>
+    
+    <button onClick= {()=>{
+      setNum(num+1);
+    }}> Click me {num} </button>
+    <br></br>
+    
+    <button onClick= {()=>{
+      setNum2(num2+1);
+    }}> Click me {num2} </button>
     </>
     );
-
-
-};
+}
 
 export default App;
-export {FirstName, LastName};
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // Now i import the new component Heading
 import App from './App';
-import './index.css';
+// import './index.css';
 
 
 ReactDOM.render( <App />,  document.getElementById('root') );
